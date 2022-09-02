db8a4cbc9b7c0f92f780f0df6cc4ec5ed54d689f
LeeJ1Hyun <nextlinehappy516@gmail.com>
2022-09-02 14:53:55 +0900

    kakao SignIn Unit Test

--- tests/kakao.test.js
+++ tests/kakao.test.js
@@ -1,50 +1,77 @@
 const request = require("supertest");
-
+const axios = require("axios");
 const { createApp } = require("../app");
 const { AppDataSource } = require("../models/dataSource");
 
-const myMock = jest.fn();
-myMock.mockReturnValue({
-  id: 1010101022,
-  connected_at: "2022-08-30T14:41:02Z",
-  properties: {
-    nickname: "아무개",
-  },
-  kakao_account: {
-    profile_nickname_needs_agreement: false,
-    profile: {
-      nickname: "아무개",
-    },
-    has_email: true,
-    email_needs_agreement: false,
-    is_email_valid: true,
-    is_email_verified: true,
-    email: "amugae0210@gmail.com",
-  },
-});
+describe("kakao SignIn", () => {
+  let app;
 
-beforeAll(async () => {
-  app = createApp();
-  await AppDataSource.initialize();
-});
+  beforeAll(async () => {
+    app = createApp();
+    await AppDataSource.initialize();
+  });
 
-afterAll(async () => {
-  await AppDataSource.query(`SET FOREIGN_KEY_CHECKS = 0`);
-  await AppDataSource.query(`TRUNCATE users`);
-  await AppDataSource.query(`SET FOREIGN_KEY_CHECKS = 1`);
-  await AppDataSource.destroy();
-});
+  afterAll(async () => {
+    await AppDataSource.query(`SET FOREIGN_KEY_CHECKS = 0`);
+    await AppDataSource.query(`TRUNCATE users`);
+    await AppDataSource.query(`SET FOREIGN_KEY_CHECKS = 1`);
+    await AppDataSource.destroy();
+  });
 
-test("FAILED: KEY_ERROR", async () => {
-  await request(app)
-    .post("/auth/signIn")
-    .send({ nickname: myMock().kakao_account.profile.nickname, email: myMock().kakao_account.email, kakaoId: myMock().id })
+  test("SUCCESS: kakao signin", async () => {
+    axios.get = jest.fn().mockReturnValue({
+      data: {
+        id: 1010101022,
+        connected_at: "2022-08-30T14:41:02Z",
+        properties: {
+          nickname: "아무개",
+        },
+        kakao_account: {
+          profile_nickname_needs_agreement: false,
+          profile: {
+            nickname: "아무개",
+          },
+          has_email: true,
+          email_needs_agreement: false,
+          is_email_valid: true,
+          is_email_verified: true,
+          email: "amugae0210@gmail.com",
+        },
+      },
+    });
+
+    await request(app)
+      .post("/auth/signIn")
+      .set({
+        Authorization: "Bearer accessToken",
+      })
+      .expect(200);
+  });
+
+  test("FAILED: kakaoToken not exist", async () => {
+    axios.get = jest.fn().mockReturnValue({
+      data: {
+        id: 1010101022,
+        connected_at: "2022-08-30T14:41:02Z",
+        properties: {
+          nickname: "아무개",
+        },
+        kakao_account: {
+          profile_nickname_needs_agreement: false,
+          profile: {
+            nickname: "아무개",
+          },
+          has_email: true,
+          email_needs_agreement: false,
+          is_email_valid: true,
+          is_email_verified: true,
+          email: "amugae0210@gmail.com",
+        },
+      },
+    });
+
+    await request(app).
+    post("/auth/signIn")
     .expect(400);
-});
-
-test("SUCCESS: kakao signin", async () => {
-  await request(app)
-  .post("/auth/signIn")
-  .send({ nickname: myMock().kakao_account.profile.nickname, email: myMock().kakao_account.email, kakaoId: myMock().id })
-  .expect(200);
+  });
 });
