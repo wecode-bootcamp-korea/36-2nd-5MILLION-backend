0de6f46f63a1e9a663d7ec7525bdb7298cb343d2
LeeJ1Hyun <nextlinehappy516@gmail.com>
2022-09-02 14:00:14 +0900

    Add : book class 기능 구현

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const authRouter = require("./authRouter");
 const instructorRouter = require("./instructorRouter");
 const accountRouter = require("./accountRouter");
-
+const bookRouter = require("./bookRouter");
 
 router.use("/auth", authRouter.router);
 router.use("/instructors", instructorRouter.router);
 router.use("/account", accountRouter.router);
+router.use("/class", bookRouter.router);
 
 module.exports = router;
