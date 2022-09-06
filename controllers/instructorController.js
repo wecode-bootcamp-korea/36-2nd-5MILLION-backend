bb085a24ba9c631d63282f3b71d94a3bb9732224
이솔 <isol@isol-ui-MacBookAir.local>
2022-09-06 14:39:11 +0900

    [ADD] instructors 조회 API 구현

--- controllers/instructorController.js
+++ controllers/instructorController.js
@@ -4,11 +4,8 @@ const getInstructors = async (req, res) => {
     try {
         const { limit, offset } = req.query;
 
-        if (!limit || !offset) {
-            return res.status(400).json({ message : "KEY_ERROR"});
-        }
         const getInstructors = await instructorService.getInstructors(limit, offset);
-        return res.status(200).json({ instructors : getInstructors});
+        return res.status(200).json({ instructors : getInstructors });
 
     } catch (err) {
         return res.status(err.statusCode || 500).json({ message : err.message });
--- models/instructorDao.js
+++ models/instructorDao.js
@@ -1,16 +1,25 @@
 const {AppDataSource} = require("./dataSource");
 
 const getInstructors = async (limit, offset) => {
+
+    if (!limit) {
+        limit = 10;
+    }
+    
+    if (!offset) {
+        offset = 0;
+    }
+
     try {
         return await AppDataSource.query(`
         SELECT
             id,
             name_en,
             profile_image_url
-        FROM instructors 
+        FROM instructors
         LIMIT ${limit} OFFSET ${offset} 
         `);
-    }
+    } 
     catch(err) {
         const error = new Error("INVALID_DATA_INPUT");
         err.statusCode = 500;
