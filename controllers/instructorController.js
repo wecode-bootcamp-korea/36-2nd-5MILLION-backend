f02b5542c35da41a3d7b579d1472aab080640788
이솔 <isol@isol-ui-MacBookAir.local>
2022-09-06 14:39:11 +0900

    리스트 최종

--- controllers/instructorController.js
+++ controllers/instructorController.js
@@ -4,9 +4,6 @@ const getInstructors = async (req, res) => {
     try {
         const { limit, offset } = req.query;
 
-        if (!limit || !offset) {
-            return res.status(400).json({ message : "KEY_ERROR"});
-        }
         const getInstructors = await instructorService.getInstructors(limit, offset);
         return res.status(200).json({ instructors : getInstructors});
 
--- models/instructorDao.js
+++ models/instructorDao.js
@@ -1,6 +1,15 @@
 const {AppDataSource} = require("./dataSource");
 
 const getInstructors = async (limit, offset) => {
+
+    if (!limit) {
+        limit = 10;
+    }
+    
+    if (!offset) {
+        offset = 0;
+    }
+
     try {
         return await AppDataSource.query(`
         SELECT
@@ -10,7 +19,7 @@ const getInstructors = async (limit, offset) => {
         FROM instructors 
         LIMIT ${limit} OFFSET ${offset} 
         `);
-    }
+    } 
     catch(err) {
         const error = new Error("INVALID_DATA_INPUT");
         err.statusCode = 500;
