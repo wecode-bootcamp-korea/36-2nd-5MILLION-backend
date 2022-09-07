019af548f9facd1c148d8f62ab35b6c79ee8e7a6
이솔 <isol@isol-ui-MacBookAir.local>
2022-09-07 17:36:48 +0900

    Modify: instructors 조회 API 수정

--- services/instructorService.js
+++ services/instructorService.js
@@ -1,10 +1,6 @@
 const instructorDao = require("../models/instructorDao");
 
 const getInstructors = async (limit = 10, offset = 0) => {
-
-    if (!limit || offset === undefined) {
-        throw new Error("KEY_ERROR", 500);
-    }
     return await instructorDao.getInstructors(limit, offset);
 };
 
